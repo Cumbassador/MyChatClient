584295cc114bfecc429230a33aca739485d98662
Cumbassador <163416241+Cumbassador@users.noreply.github.com>
2024-06-09 14:22:08 +0300

    Version 1.0

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import lombok.SneakyThrows;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Main {
+    public final static int PORT = 8081;
+    public final static String HOST = "localhost";
+    @SneakyThrows
+    public static void main(String[] args) {
+        Socket socket = new Socket(HOST,PORT);
+
+        if (socket.isConnected()){
+            PrintWriter serverWriter = new PrintWriter(socket.getOutputStream());
+            serverWriter.println("Hi from client");
+            serverWriter.flush();
+        }
+    }
+}
